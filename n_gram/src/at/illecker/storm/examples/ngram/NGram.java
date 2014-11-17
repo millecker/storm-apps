dfa26f45de1004637912afb29b412de6500c7663
millecker <martin.illecker@gmail.com>
2014-11-17 13:25:30 +0100

    Update NGram application

--- n_gram/src/at/illecker/storm/examples/ngram/NGram.java
+++ n_gram/src/at/illecker/storm/examples/ngram/NGram.java
@@ -16,6 +16,11 @@
  */
 package at.illecker.storm.examples.ngram;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+
 /**
  * N-Gram Java implementations
  * 
@@ -24,6 +29,66 @@ public class NGram {
 
   public static void main(String[] args) {
 
+    List<String> tweets = new ArrayList<String>();
+    tweets.add("Colorless green ideas sleep furiously");
+
+    for (String tweet : tweets) {
+      // Cleanup Tweet (remove @user #hashtag RT)
+      // \\n - newlines
+      // @\\w* - @users
+      // #\\w* - hashtags
+      // \\bRT\\b - Retweets RT
+      // \\p{Punct} - removes smileys
+      // [^@#\\p{L}\\p{N} ]+
+      // URL
+      // (https?:\\/\\/)?([\\da-z\\.-]+)\\.([a-z\\.]{2,6})([\\/\\w \\.-]*)*\\/?
+      // -
+      Pattern pattern = Pattern
+          .compile("\\n|@\\w*|#\\w*|\\bRT\\b|"
+              + "(https?:\\/\\/)?([\\da-z\\.-]+)\\.([a-z\\.]{2,6})([\\/\\w \\.-]*)*\\/?");
+      String cleanupTweet = pattern.matcher(tweet).replaceAll(" ");
+
+      // Tweet to lower case
+      cleanupTweet = cleanupTweet.toLowerCase();
+      System.out.println("Tweet: " + cleanupTweet);
+
+      String[] words = cleanupTweet.split(" ");
+
+      // Bigrams
+      List<String[]> bigrams = getBigrams(words);
+      for (int i = 0; i < bigrams.size(); i++) {
+        System.out.println("bigram[" + i + "]: "
+            + Arrays.toString(bigrams.get(i)));
+      }
+
+      // Trigrams
+      List<String[]> trigrams = getTrigrams(words);
+      for (int i = 0; i < trigrams.size(); i++) {
+        System.out.println("trigrams[" + i + "]: "
+            + Arrays.toString(trigrams.get(i)));
+      }
+
+    }
+  }
+
+  public static List<String[]> getBigrams(String[] words) {
+    List<String[]> bigrams = new ArrayList<String[]>();
+    for (int i = 0; i < words.length; i++) {
+      if (i < words.length - 1) {
+        bigrams.add(new String[] { words[i], words[i + 1] });
+      }
+    }
+    return bigrams;
+  }
+
+  public static List<String[]> getTrigrams(String[] words) {
+    List<String[]> trigrams = new ArrayList<String[]>();
+    for (int i = 0; i < words.length; i++) {
+      if (i < words.length - 2) {
+        trigrams.add(new String[] { words[i], words[i + 1], words[i + 2] });
+      }
+    }
+    return trigrams;
   }
 
 }
--- n_gram/src/at/illecker/storm/examples/ngram/bolt/BiGramBolt.java
+++ n_gram/src/at/illecker/storm/examples/ngram/bolt/BiGramBolt.java
@@ -16,18 +16,23 @@
  */
 package at.illecker.storm.examples.ngram.bolt;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import twitter4j.Status;
+import at.illecker.storm.examples.ngram.NGram;
 import backtype.storm.task.OutputCollector;
 import backtype.storm.task.TopologyContext;
 import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.topology.base.BaseRichBolt;
 import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
 
 public class BiGramBolt extends BaseRichBolt {
   private static final long serialVersionUID = 7554959448481890289L;
@@ -50,7 +55,40 @@ public class BiGramBolt extends BaseRichBolt {
     LOG.info("User: " + status.getUser().getScreenName() + " Tweet: "
         + tweetText);
 
-    // this.m_collector.emit(new Values(sentence));
+    // Cleanup Tweet (remove @user #hashtag RT)
+    // \\n - newlines
+    // @\\w* - @users
+    // #\\w* - hashtags
+    // \\bRT\\b - Retweets RT
+    // \\p{Punct} - removes smileys
+    // [^@#\\p{L}\\p{N} ]+
+    // URL
+    // (https?:\\/\\/)?([\\da-z\\.-]+)\\.([a-z\\.]{2,6})([\\/\\w \\.-]*)*\\/?
+    // -
+    Pattern pattern = Pattern
+        .compile("\\n|@\\w*|#\\w*|\\bRT\\b|"
+            + "(https?:\\/\\/)?([\\da-z\\.-]+)\\.([a-z\\.]{2,6})([\\/\\w \\.-]*)*\\/?");
+    String cleanupTweet = pattern.matcher(tweetText).replaceAll(" ");
+
+    // Tweet to lower case
+    cleanupTweet = cleanupTweet.toLowerCase();
+    LOG.info("Tweet: " + cleanupTweet);
+
+    String[] words = cleanupTweet.split(" ");
+
+    // Bigrams
+    List<String[]> bigrams = NGram.getBigrams(words);
+    for (int i = 0; i < bigrams.size(); i++) {
+      LOG.info("bigram[" + i + "]: " + Arrays.toString(bigrams.get(i)));
+    }
+
+    // Trigrams
+    List<String[]> trigrams = NGram.getTrigrams(words);
+    for (int i = 0; i < trigrams.size(); i++) {
+      LOG.info("trigrams[" + i + "]: " + Arrays.toString(trigrams.get(i)));
+    }
+
+    this.m_collector.emit(new Values(bigrams));
     this.m_collector.ack(tuple);
   }
 }
