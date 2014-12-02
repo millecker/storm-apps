d49d055e90b5b06c5d0bd7a092d7111621840af4
millecker <martin.illecker@gmail.com>
2014-12-02 16:01:15 +0100

    Rename to JsonTweetExtractorBol

--- sentiment_analysis/src/at/illecker/storm/examples/sentimentanalysis/SentimentAnalysisTopology.java
+++ sentiment_analysis/src/at/illecker/storm/examples/sentimentanalysis/SentimentAnalysisTopology.java
@@ -19,9 +19,9 @@ package at.illecker.storm.examples.sentimentanalysis;
 import java.io.File;
 import java.util.Arrays;
 
+import at.illecker.storm.examples.sentimentanalysis.bolt.JsonTweetExtractorBolt;
 import at.illecker.storm.examples.sentimentanalysis.bolt.POSTaggerBolt;
 import at.illecker.storm.examples.sentimentanalysis.bolt.PolarityDetectionBolt;
-import at.illecker.storm.examples.sentimentanalysis.bolt.PreprocessorBolt;
 import at.illecker.storm.examples.sentimentanalysis.bolt.SentenceSplitterBolt;
 import at.illecker.storm.examples.util.spout.JsonFileSpout;
 import at.illecker.storm.examples.util.spout.TwitterSpout;
@@ -40,7 +40,7 @@ import backtype.storm.topology.TopologyBuilder;
 public class SentimentAnalysisTopology {
 
   private static final String TWEET_SPOUT_ID = "tweet-spout";
-  private static final String PREPROCESSOR_BOLT_ID = "preprocessor-bolt";
+  private static final String JSON_TWEET_EXTRACTOR_BOLT_ID = "json-tweet-extractor-bolt";
   private static final String SENTENCE_SPLITTER_BOLT_ID = "sentence-splitter-bolt";
   private static final String POS_TAGGER_BOLT_ID = "pos-tagger-bolt";
   private static final String POLARITY_DETECTION_BOLT_ID = "polarity-detection-bolt";
@@ -111,7 +111,7 @@ public class SentimentAnalysisTopology {
     }
 
     // Create Bolts
-    PreprocessorBolt preprocessorBolt = new PreprocessorBolt();
+    JsonTweetExtractorBolt jsonTweetExtractorBolt = new JsonTweetExtractorBolt();
     SentenceSplitterBolt sentenceSplitterBolt = new SentenceSplitterBolt();
     POSTaggerBolt posTaggerBolt = new POSTaggerBolt();
     PolarityDetectionBolt polarityDetectionBolt = new PolarityDetectionBolt();
@@ -122,13 +122,13 @@ public class SentimentAnalysisTopology {
     // Set Spout
     builder.setSpout(TWEET_SPOUT_ID, spout);
 
-    // Set Spout --> PreprocessorBolt
-    builder.setBolt(PREPROCESSOR_BOLT_ID, preprocessorBolt).shuffleGrouping(
-        TWEET_SPOUT_ID);
+    // Set Spout --> JsonTweetExtractorBolt
+    builder.setBolt(JSON_TWEET_EXTRACTOR_BOLT_ID, jsonTweetExtractorBolt)
+        .shuffleGrouping(TWEET_SPOUT_ID);
 
     // TweetFeatureExtractorBolt --> SentenceSplitterBolt
     builder.setBolt(SENTENCE_SPLITTER_BOLT_ID, sentenceSplitterBolt)
-        .shuffleGrouping(PREPROCESSOR_BOLT_ID);
+        .shuffleGrouping(JSON_TWEET_EXTRACTOR_BOLT_ID);
 
     // SentenceSplitterBolt --> POSTaggerBolt
     builder.setBolt(POS_TAGGER_BOLT_ID, posTaggerBolt).shuffleGrouping(
