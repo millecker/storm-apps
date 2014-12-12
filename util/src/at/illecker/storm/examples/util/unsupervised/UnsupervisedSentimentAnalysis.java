9981850b166886a7aa674b2ff68bcf471b934cbd
millecker <martin.illecker@gmail.com>
2014-12-12 10:33:52 +0100

    Update unsupervised sentiment analysis

--- util/src/at/illecker/storm/examples/util/unsupervised/UnsupervisedSentimentAnalysis.java
+++ util/src/at/illecker/storm/examples/util/unsupervised/UnsupervisedSentimentAnalysis.java
@@ -19,12 +19,18 @@ package at.illecker.storm.examples.util.unsupervised;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import at.illecker.storm.examples.util.Tweet;
 import at.illecker.storm.examples.util.unsupervised.sentiwordnet.SentiWordNet;
+import at.illecker.storm.examples.util.unsupervised.wordnet.POSTag;
 import at.illecker.storm.examples.util.unsupervised.wordnet.WordNet;
+import edu.mit.jwi.item.POS;
+import edu.stanford.nlp.ling.HasWord;
+import edu.stanford.nlp.ling.TaggedWord;
 
 public class UnsupervisedSentimentAnalysis {
 
@@ -33,21 +39,21 @@ public class UnsupervisedSentimentAnalysis {
       + File.separator
       + "resources"
       + File.separator
-      + "wordlists" + File.separator + "AFINN-111.txt";
+      + "wordlists"
+      + File.separator
+      + "SentStrength_Data_Sept2011_EmotionLookupTable.txt";
   public static final String SENTIMENT_WORD_LIST2 = System
       .getProperty("user.dir")
       + File.separator
       + "resources"
       + File.separator
-      + "wordlists"
-      + File.separator
-      + "SentStrength_Data_Sept2011_EmotionLookupTable.txt";
+      + "wordlists" + File.separator + "AFINN-111.txt";
   private static final Logger LOG = LoggerFactory
       .getLogger(UnsupervisedSentimentAnalysis.class);
 
   private static UnsupervisedSentimentAnalysis instance = new UnsupervisedSentimentAnalysis();
-  private static WordNet m_wordnet;
-  private static SentiWordNet m_sentiwordnet;
+  private WordNet m_wordnet;
+  private SentiWordNet m_sentiwordnet;
   // AFINN word list (minValue -5 and maxValue +5)
   private WordListMap<Double> m_wordList1;
   // SentStrength word list (minValue -5 and maxValue +5)
@@ -70,6 +76,78 @@ public class UnsupervisedSentimentAnalysis {
 
   public static UnsupervisedSentimentAnalysis getInstance() {
     return instance;
+  }
+
+  public Double getWordSentiment(String word) {
+    // check SentStrength word list
+    Double sentimentScore = null;
+    if (m_wordList1 != null) {
+      // get normalized score between 0 and 1
+      sentimentScore = m_wordList1.matchKey(word, true);
+    }
+    // check AFINN word list
+    if (sentimentScore == null) {
+      if (m_wordList2 != null) {
+        // get normalized score between 0 and 1
+        sentimentScore = m_wordList2.matchKey(word, true);
+      }
+    }
+    LOG.info("getWordSentiment('" + word + "'): " + sentimentScore);
+    return sentimentScore;
+  }
+
+  public Double getWordSentimentWithStemming(String word, POS posTag) {
+    Double sentimentScore = getWordSentiment(word);
+    // use word stemming
+    if (sentimentScore == null) {
+      List<String> stemmedWords = m_wordnet.findStems(word, posTag);
+      for (String stemmedWord : stemmedWords) {
+        if (!stemmedWord.equals(word)) {
+          sentimentScore = getWordSentiment(stemmedWord);
+        }
+        if (sentimentScore != null) {
+          break;
+        }
+      }
+    }
+    return sentimentScore;
+  }
+
+  public double getSentenceSentiment(List<HasWord> sentence) {
+    double sentenceScore = 0;
+    int count = 0;
+    for (HasWord w : sentence) {
+      String word = w.word().toLowerCase().trim();
+      Double wordSentiment = getWordSentiment(word);
+      if (wordSentiment != null) {
+        sentenceScore += wordSentiment;
+        count++;
+      }
+    }
+    return (count > 0) ? sentenceScore / count : 0;
+  }
+
+  public double getTaggedSentenceSentiment(List<TaggedWord> sentence) {
+    double sentenceScore = 0;
+    int count = 0;
+    for (TaggedWord w : sentence) {
+      String word = w.word().toLowerCase().trim();
+      Double wordSentiment = getWordSentimentWithStemming(word,
+          POSTag.convertString(w.tag()));
+      if (wordSentiment != null) {
+        sentenceScore += wordSentiment;
+        count++;
+      }
+    }
+    return (count > 0) ? sentenceScore / count : 0;
+  }
+
+  public double getTweetSentiment(Tweet tweet) {
+    double totalScore = 0;
+    for (List<TaggedWord> sentence : tweet.getTaggedSentences()) {
+      totalScore += getTaggedSentenceSentiment(sentence);
+    }
+    return totalScore;
   }
 
   public void close() {
