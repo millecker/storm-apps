b34069cc8fbd7c3aea15f7c7ed410387fd549c4b
millecker <martin.illecker@gmail.com>
2014-12-26 16:23:06 +0100

    Update IOUtils including getInputStream

--- util/src/at/illecker/storm/examples/util/io/IOUtils.java
+++ util/src/at/illecker/storm/examples/util/io/IOUtils.java
@@ -16,18 +16,28 @@
  */
 package at.illecker.storm.examples.util.io;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.zip.GZIPInputStream;
 
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
+import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class IOUtils {
+  private static final int EXTRACT_BUFFER_SIZE = 2048;
+  private static final int GZIP_FILE_BUFFER_SIZE = 65536;
   private static final Logger LOG = LoggerFactory.getLogger(IOUtils.class);
-
-  // TODO
-  // https://github.com/stanfordnlp/CoreNLP/blob/master/src/edu/stanford/nlp/io/IOUtils.java
-  // getInputStreamFromURLOrClasspathOrFileSystem
 
   public static void delete(File file) throws IOException {
     IOUtils.delete(file, false);
@@ -46,4 +56,83 @@ public class IOUtils {
     }
   }
 
+  public static InputStream getInputStream(String fileOrUrl) {
+    InputStream in = null;
+    try {
+      if (fileOrUrl.matches("https?://.*")) {
+        URL u = new URL(fileOrUrl);
+        URLConnection uc = u.openConnection();
+        in = uc.getInputStream();
+      } else {
+        // 1) check if file is within jar
+        in = IOUtils.class.getClassLoader().getResourceAsStream(fileOrUrl);
+
+        // windows File.separator is \, but getting resources only works with /
+        if (in == null) {
+          in = IOUtils.class.getClassLoader().getResourceAsStream(
+              fileOrUrl.replaceAll("\\\\", "/"));
+        }
+
+        // 2) if not found in jar, load from the file system
+        if (in == null) {
+          in = new FileInputStream(fileOrUrl);
+        }
+      }
+
+      // unzip if necessary
+      if (fileOrUrl.endsWith(".gz")) {
+        in = new GZIPInputStream(in, GZIP_FILE_BUFFER_SIZE);
+      }
+
+      // buffer input stream
+      in = new BufferedInputStream(in);
+
+    } catch (FileNotFoundException e) {
+      LOG.error("FileNotFoundException: " + e.getMessage());
+    } catch (IOException e) {
+      LOG.error("IOException: " + e.getMessage());
+    }
+
+    return in;
+  }
+
+  public static void extractTarGz(String inputTarGz, String outDir)
+      throws IOException {
+    extractTarGz(getInputStream(inputTarGz), outDir, false);
+  }
+
+  public static void extractTarGz(InputStream inputTarGzStream, String outDir,
+      boolean logging) throws IOException {
+
+    BufferedInputStream in = new BufferedInputStream(inputTarGzStream);
+    GzipCompressorInputStream gzIn = new GzipCompressorInputStream(in);
+    TarArchiveInputStream tarIn = new TarArchiveInputStream(gzIn);
+
+    // read Tar entries
+    TarArchiveEntry entry = null;
+    while ((entry = (TarArchiveEntry) tarIn.getNextEntry()) != null) {
+      if (logging) {
+        LOG.info("Extracting: " + outDir + File.separator + entry.getName());
+      }
+      if (entry.isDirectory()) { // create directory
+        File f = new File(outDir + File.separator + entry.getName());
+        f.mkdirs();
+      } else { // decompress file
+        int count;
+        byte data[] = new byte[EXTRACT_BUFFER_SIZE];
+
+        FileOutputStream fos = new FileOutputStream(outDir + File.separator
+            + entry.getName());
+        BufferedOutputStream dest = new BufferedOutputStream(fos,
+            EXTRACT_BUFFER_SIZE);
+        while ((count = tarIn.read(data, 0, EXTRACT_BUFFER_SIZE)) != -1) {
+          dest.write(data, 0, count);
+        }
+        dest.close();
+      }
+    }
+
+    // close input stream
+    tarIn.close();
+  }
 }
