14043d05bde24e404015309ead8f44cad4c39e96
millecker <martin.illecker@gmail.com>
2015-02-24 17:59:59 +0100

    Fix TfType again

--- commons/src/at/illecker/storm/commons/svm/SVM.java
+++ commons/src/at/illecker/storm/commons/svm/SVM.java
@@ -514,16 +514,16 @@ public class SVM {
         } else if (featureVectorGenerator
             .equals(TfIdfFeatureVectorGenerator.class)) {
           TweetTfIdf tweetTfIdf = TweetTfIdf.createFromTaggedTokens(
-              taggedTweets, TfType.RAW, TfIdfNormalization.COS, true);
+              taggedTweets, TfType.LOG, TfIdfNormalization.COS, true);
           LOG.info("Load TfIdfFeatureVectorGenerator...");
           fvg = new TfIdfFeatureVectorGenerator(tweetTfIdf);
 
         } else if (featureVectorGenerator
             .equals(CombinedFeatureVectorGenerator.class)) {
           TweetTfIdf tweetTfIdf = TweetTfIdf.createFromTaggedTokens(
-              taggedTweets, TfType.RAW, TfIdfNormalization.COS, true);
+              taggedTweets, TfType.LOG, TfIdfNormalization.COS, true);
           LOG.info("Load CombinedFeatureVectorGenerator...");
-          fvg = new CombinedFeatureVectorGenerator(false, true, tweetTfIdf);
+          fvg = new CombinedFeatureVectorGenerator(true, true, tweetTfIdf);
 
         } else {
           throw new UnsupportedOperationException("FeatureVectorGenerator '"
